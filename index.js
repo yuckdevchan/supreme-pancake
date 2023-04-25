933eb666ee70c43b2c24aff2147b0bd0407dd6dc
manSlayer16 <120093141+manSlayer16@users.noreply.github.com>
2023-04-25 14:38:17 +0100

    heheheha

--- index.js
+++ index.js
@@ -2,6 +2,12 @@ var clicks = 0
 var clickMod = 1 * Multiplyer
 var Multiplyer = 1
 
+function addClicker() {
+  if clicks => 10:
+    Multiplyer += 1
+  else:
+    console.log("Bad")
+}
 function click() {
   clicks += clickMod
 }
